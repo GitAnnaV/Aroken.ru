be5fa05b6a5a820b7a65cb6250b39a541022eef6
GitAnnaV <annavirasovaa@yandex.ru>
2025-07-02 19:24:20 +1000

    Update main.js

--- lesson 113/main.js
+++ lesson 113/main.js
@@ -17,9 +17,9 @@ btn.addEventListener('click', openModal)
 modal.addEventListener('click', event => {
     const target = event.target
 
-    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
+    if (target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
         closeModal()
-    }
+}
 })
 
 document.addEventListener('keydown', event => {
